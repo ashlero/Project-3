cadac873efd090c4647e4003d8b65c96822a3091
ashlero <alerouge1004@gmail.com>
2026-04-04 20:33:14 -0500

    adjusted positioning of elements and added music and styling

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
+
 const pastries = document.querySelectorAll(".clickable");
+const resetBtn = document.getElementById("resetBtn");
+
+let bittenCount = 0;
 
 pastries.forEach((pastry) => {
   const image = pastry.querySelector("img");
@@ -17,38 +21,64 @@ pastries.forEach((pastry) => {
       return;
     }
 
-    if (isBitten) {
-      image.src = defaultImage;
-      isBitten = false;
-    } else {
-      image.src = biteImage;
-      isBitten = true;
+    if (isBitten) return;
+
+    image.src = biteImage;
+    isBitten = true;
+    pastry.classList.add("bitten");
+    bittenCount++;
+
+    if (bittenCount === pastries.length) {
+      resetBtn.classList.add("show");
     }
   });
+
+  pastry.resetPastry = () => {
+    image.src = defaultImage;
+    isBitten = false;
+    firstTap = true;
+    pastry.classList.remove("bitten", "show-tag");
+  };
 });
 
-document.querySelectorAll(".pastry").forEach((pastry) => {
-  const biteSrc = pastry.dataset.bite;
-
-  pastry.addEventListener("click", () => {
-    if (pastry.classList.contains("bitten")) return;
-
-    pastry.src = biteSrc;
-    pastry.classList.add("bitten");
+resetBtn.addEventListener("click", () => {
+  pastries.forEach((pastry) => {
+    pastry.resetPastry();
   });
+
+  bittenCount = 0;
+  resetBtn.classList.remove("show");
 });
 
 const music = document.getElementById("bgMusic");
 const toggleBtn = document.getElementById("musicToggle");
 
-toggleBtn.addEventListener("click", () => {
+toggleBtn.classList.add("muted");
 
-  if (music.paused) {
-    music.play();
-    toggleBtn.classList.remove("muted");
-  } else {
-    music.pause();
-    toggleBtn.classList.add("muted");
+toggleBtn.addEventListener("click", async () => {
+  try {
+    if (music.paused) {
+      await music.play();
+      toggleBtn.classList.remove("muted");
+    } else {
+      music.pause();
+      toggleBtn.classList.add("muted");
+    }
+  } catch (error) {
+    console.log("Playback failed:", error);
   }
-
 });
+
+const title = document.querySelector(".bounce-title");
+
+title.innerHTML = title.textContent
+  .split("")
+  .map((letter, i, arr) => {
+    const isLast = i === arr.length - 1;
+    const delay = isLast ? (i * 0.08) + 0.25 : i * 0.08;
+
+    return `<span style="animation-delay:${delay}s">${
+      letter === " " ? "&nbsp;" : letter
+    }</span>`;
+  })
+  .join("");
